26de59833eb2dc3311a5f4dad933bc65abb35281
davinawuy <davin.awuy@gmail.com>
2026-03-14 14:49:21 +1000

    feat: Incorporate Translate Function

---
+++ src/hooks/useTranslation.js
@@ -0,0 +1,12 @@
+import { useContext } from 'react'
+import { TranslationContext } from '../components/TranslationContext'
+
+export function useTranslation() {
+  const context = useContext(TranslationContext)
+
+  if (!context) {
+    throw new Error('useTranslation must be used within TranslationProvider.')
+  }
+
+  return context
+}
